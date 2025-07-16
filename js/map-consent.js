f291007b15493bad0fd384b099d5ef83918b602d
espacetrinite <espacetrinite@wanadoo.fr>
2025-07-16 11:35:57 +0200

    Update map-consent.js

--- js/map-consent.js
+++ js/map-consent.js
@@ -10,12 +10,10 @@ window.addEventListener("load", function () {
     iframe.style.display = "block";
   }
 
-  // Chargement automatique si consentement déjà donné
   if (localStorage.getItem("mapAccepted")) {
     showMap();
   }
 
-  // Affichage après clic
   button.addEventListener("click", function (e) {
     e.preventDefault();
     localStorage.setItem("mapAccepted", "true");
